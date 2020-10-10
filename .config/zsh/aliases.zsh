f28e8a280b43a13839b5df2c9cd2c4bf4e2c6c36
Henry Jen <henry.jen@oracle.com>
2020-10-10 16:53:52 -0700

    Use real filenames and add i3 config

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias lslo="find . -name '*.orig' -o -name '*.rej'"
 alias cleanup="find . \( -name '*.orig' -o -name '*.rej' \) -exec rm {} \;"
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
--- setup.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16/shell
